949d3b06509bc67126fe8ddb8538955a5e83ccf4
Chris Wallace <wallacecm@gmail.com>
2019-06-11 17:42:55 -0400

    Update init_pi.sh

--- init_pi.sh
+++ init_pi.sh
@@ -7,3 +7,7 @@ sed -i '/en_GB.UTF-8/s/^/# /' /etc/locale.gen
 sed -i '/en_US.UTF-8/s/# //' /etc/locale.gen
 locale-gen
 
+echo 'updating keyboard...'
+sed -i '/XKBMODEL/s/"pc105"/"pc101"/' /etc/default/keyboard
+sed -i '/XKBLAYOUT/s/"gb"/"us"/' /etc/default/keyboard
+
